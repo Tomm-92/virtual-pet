e8e9e771e3613afb2dbe572fb558057b40f1cc57
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2022-12-13 21:22:40 +0000

    currently working on walk dog function

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,13 +4,39 @@ describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
-  });
 
-  describe('constructor', () => {
-    
     it('sets the name property', () => {
       const pet = new Pet('Fido');
   
       expect(pet.name).toEqual('Fido');
     });
+
+    it('has an intial age of 0', () => {
+      const pet = new Pet('Fido');
+  
+      expect(pet.age).toEqual(0);
+    });
+
   });
+
+  describe('growUp', () => {
+    it('increments the age by 1', () => {
+      const pet = new Pet('Fido');
+
+      pet.growUp();
+      expect(pet.age).toEqual(1);
+      expect(pet.hunger).toEqual(5);
+      expect(pet.fitness).toEqual(7);
+    });  
+  });
+
+  describe('walkPet', () => {
+    it('increments the fitness by 4', () => {
+      const pet = new Pet('Fido');
+
+      pet.fitness = 4;
+      pet.walk();
+      expect(pet.fitness).toEqual(8);
+    });  
+  });
+
